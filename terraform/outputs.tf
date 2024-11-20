89420db787a790517753315b1ebcbdd13bab40c6
Alex Harvey <alexharv074@gmail.com>
2024-11-20 16:17:43 +1100

    Add comment

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,6 +3,13 @@
 //
 // Interpreting this to mean:
 // "Create an output.tf file that would provide an output in the form of a map, where the keys are the instance names and the values are the corresponding VM hostnames."
+
+// Note about use of predicted VM names
+//
+// I am unhappy about having to predict VM hostnames based on a pattern and repeat that code in 3 places.
+// Ideally, I would like to export that VM hostname from the compute resource as we do with the IP. Based
+// on docs however, I can't see how that is possible. I also don't have access to Google Cloud to test.
+// https://registry.terraform.io/providers/hashicorp/google/3.45.0/docs/resources/compute_instance
 output "vm_hostnames" {
   value = { for instance, config in var.instances : instance => "${instance}-${config.zone}.asx.com.au" }
 }
